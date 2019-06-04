78979d0bfd926c124c27e58b891637f669b2e5cb
bradger68 <bradger68@users.noreply.github.com>
2019-06-04 15:15:35 -0400

    Update dailyproblem70.py

--- dailyproblem70.py
+++ dailyproblem70.py
@@ -4,6 +4,29 @@ Given a positive integer n, return the n-th perfect number.
 
 For example, given 1, you should return 19. Given 2, you should return 28."""
 
+def find_nth_perfnum(n):
+  temporary_sum = 0
+  for digit in str(n):
+    temporary_sum += int(digit)
+
+  return (n * 10) + (10- temporary_sum)
+
+
+print(find_nth_perfnum(1))
+print(find_nth_perfnum(2))
+print(find_nth_perfnum(19))
+
+
+
+
+# The following function just verifies whether or not
+# an integer is perfect or not. It does not find 
+# the nth perfect number. I originally started 
+# coding it out like this but realized there was 
+# an easier way to do it after looking at solution 
+# from github user Vineetjohn
+
+
 def is_perfect(number):
   number = str(number)
   digits = []
@@ -12,6 +35,7 @@ def is_perfect(number):
   sum_of_digits = sum(digits)
   if sum_of_digits == 10:
     return True
+
 
 
 print(is_perfect(19))
