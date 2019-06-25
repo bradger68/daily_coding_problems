7c421dfda8981a0e9477df0e57d484d6260a9a64
bradger68 <bradger68@users.noreply.github.com>
2019-06-25 15:01:46 -0400

    Add files via upload

---
+++ dailycodingprob91.py
@@ -0,0 +1,11 @@
+""" What does the below code snippet print out?
+How can we fix the anonymous functions to behave as we'd expect?"""
+
+functions = []
+for i in range(10):
+    functions.append(lambda : i)
+
+i=0
+for f in functions:
+    print(f())
+    i += 1
